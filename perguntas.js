771a1cc55b321bed5dbb5af98380d7d15bbb7aa4
MestreKleber <klebermilani.pr.gov.br@escola.com>
2025-07-06 20:18:43 -0300

    final

--- perguntas.js
+++ perguntas.js
@@ -22,6 +22,42 @@ criaCartao(
 criaCartao(
     'Ed Motta',
     'O que acontece com a mexida do técnico?',
-    'Pra imitar o som da bateria eletrônica... sei lá que inferno é isso, bicho!',
+    'Pra imitar o timbre da bateria eletrônica... sei lá que inferno é isso, bicho!',
     'assets/audio/d.mp3'
 )
+
+criaCartao(
+    'Ed Motta',
+    'O técnico já mexeu na bateria?',
+    'O técnico ainda não mexeu na bateria, tá só o baterista no palco.',
+    'assets/audio/e.mp3'
+)
+
+criaCartao(
+    'Ed Motta',
+    'Como está o som da bateria?',
+    'O som tá lindo.',
+    'assets/audio/f.mp3'
+)
+
+criaCartao(
+    'Ed Motta',
+    'O técnico vai fazer o que?',
+    'O técnico vai começar a mexer na bateria.',
+    'assets/audio/g.mp3'
+)
+
+criaCartao(
+    'Ed Motta',
+    'Agora vem o quê?',
+    'Agora vem o tom tom..',
+    'assets/audio/h.mp3'
+)
+
+criaCartao(
+    'Ed Motta',
+    'O que fazer com os condimentos?',
+    'Se tem ketchups, e tem azeite é pra botar em cima, não importa como, né?',
+    'assets/audio/i.mp3'
+)
+
